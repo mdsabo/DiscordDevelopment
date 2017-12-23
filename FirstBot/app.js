ab61c633d5fd559ceda5d92451d8bed6d9ed13cc
Matthew Sabo <slab.o101@gmail.com>
2017-12-23 05:46:45 -0500

    FirstBot Development
    Added several commands to FirstBot
    Updated discord-init app boilerplate

--- FirstBot/app.js
+++ FirstBot/app.js
@@ -1,15 +1,51 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const config = require("./config.json")
+const config = require("./config.json");
+const fs = require("fs");
 
 client.on("ready", () => {
     console.log("I am ready.");
 });
 
 client.on("message", (message) => {
-    if (message.content.startsWith("ping"))
+
+	if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+
+	const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+	const cmd = args.shift().toLowerCase();
+
+    if (cmd === 'ping')
     {
         message.channel.send("pong!");
+    }
+    else if (cmd === 'pong')
+    {
+        message.channel.send("bar!");
+    }
+    else if (cmd === 'prefix')
+    {
+    	config.prefix = args[0];
+    	fs.writeFile("./config.json", JSON.stringify(config), (err)=>console.error);
+    }
+    else if (cmd === 'kick')
+    {
+    	let member = message.mentions.first();
+    	let reason = args.slice(1).join(" ");
+    	member.kick(reason);
+    }
+    else if (cmd === 'say')
+    {
+    	let text = args.slice(1).join(" ");
+    	message.delete();
+    	message.channel.send(text);
+    }
+    
+
+    if (message.author.id !== config.ownerID) return;
+    //Owner exclusive commands
+	if (cmd === 'owner')
+    {
+        message.channel.send("take me to your leader!");
     }
 });
 
--- discord-init.ps1
+++ discord-init.ps1
@@ -10,17 +10,33 @@ npm i -S discord.js
 
 $app_boiler = "const Discord = require(""discord.js"");
 const client = new Discord.Client();
-const config = require(""./config.json"")
+const config = require(""./config.json"");
+const fs = require(""fs"");
 
 client.on(""ready"", () => {
     console.log(""I am ready."");
 });
 
 client.on(""message"", (message) => {
-    if (message.content.startsWith(""ping""))
+
+    if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+
+    switch(cmd)
     {
+    case ""ping"":
         message.channel.send(""pong!"");
+        break;
+    case ""help"":
+        //Show help text
+        break;
     }
+
+    if (message.author.id !== config.ownerID) return;
+    //Owner exclusive commands
+
 });
 
 client.login(config.token);"
@@ -29,7 +45,9 @@ New-Item -Path . -Name "app.js" -ItemType "file"
 Out-File -FilePath "app.js" -InputObject $app_boiler -Encoding ASCII
 
 $cfg_boiler = "{
-  ""token"": ""insert-bot-token-here""
+  ""token"": ""insert-bot-token-here"",
+  ""prefix"": ""insert-prefix"",
+  ""ownerID"": ""insert-username""
 }"
 
 New-Item -Path . -Name "config.json" -ItemType "file"
